12d09eca8ea949afc48b98b4b6d93423e62072eb
ralisin <buniymassimo@gmail.com>
2025-02-06 18:57:16 +0100

    Completed Task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -16,10 +16,8 @@ public_users.get("/", function (req, res) {
 
 // Get book details based on ISBN
 public_users.get("/isbn/:isbn", function (req, res) {
-  //Write your code here
-
   let isbn = req.params.isbn;
-  
+
   if (books[isbn] !== undefined) {
     return res.status(200).json(books[isbn]);
   } else {
@@ -29,8 +27,17 @@ public_users.get("/isbn/:isbn", function (req, res) {
 
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  let author = req.params.author;
+
+  let author_books = Object.values(books).filter((book) => book.author === author);
+
+  console.log(author_books);
+
+  if (author_books.length > 0) {
+    return res.status(200).json(author_books);
+  } else {
+    return res.status(404).json({ message: "Author not found" });
+  }
 });
 
 // Get all books based on title
