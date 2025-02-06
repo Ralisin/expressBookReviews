60b371a2d742c21d0640447f54f1f497f869264e
ralisin <buniymassimo@gmail.com>
2025-02-06 22:47:08 +0100

    Completed Task 7

--- final_project/index.js
+++ final_project/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const app = express();
 
 const jwt = require("jsonwebtoken");
 const session = require("express-session");
@@ -7,57 +8,36 @@ const session = require("express-session");
 const customer_routes = require("./router/auth_users.js").authenticated;
 const genl_routes = require("./router/general.js").general;
 
-// Functions
-const is_valid = require("./router/auth_users.js").isValid;
+// Import from env file
+const JWT_SECRET_KEY = "fingerprint_customer";
 
-// Data
-const users = require("./router/auth_users.js").users;
-
-const app = express();
+const PORT = 5000;
 
 app.use(express.json());
 
 app.use(
   "/customer",
   session({
-    secret: "fingerprint_customer",
+    secret: JWT_SECRET_KEY,
     resave: true,
     saveUninitialized: true,
   })
 );
 
 app.use("/customer/auth/*", function auth(req, res, next) {
-  if (req.session.token) {
-    jwt.verify(req.session.token, "fingerprint_customer", (err, user) => {
-      if (err) return res.status(403).json({ message: "Forbidden" });
+  console.log(req.session.authorization);
 
-      req.user = user;
-      next();
-    });
-  } else {
-    return res.status(403).json({ message: "Forbidden" });
-  }
+  let token = req.session.authorization['token'];
+
+  if (!token) return res.status(403).json({ message: "Forbidden" });
+
+  jwt.verify(token, TOKEN_HEADER_KEY, (err, user) => {
+    if (err) return res.status(403).json({ message: "User not logged in" });
+
+    req.user = user;
+    next();
+  });
 });
-
-// Register a user
-app.post("/register", (req,res) => {
-  let username = req.body.username;
-  let password = req.body.password;
-
-  if(!username || !password) {
-    return res.status(400).json({message: "Invalid username or password"});
-  }
-
-  if(is_valid(username)) {
-    return res.status(400).json({message: "Username already exists"});
-  }
-
-  users.push({username: username, password: password});
-
-  return res.status(200).json({ message: "User successfully registered. Now you can login" });
-});
-
-const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,35 +1,72 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 let books = require("./booksdb.js");
+
 const regd_users = express.Router();
+
+const TOKEN_HEADER_KEY = "gfg_token_header_key";
 
 let users = [];
 
 // Check if username is valid
-const isValid = (username)=>{ //returns boolean
+const isValid = (username) => {
+  //returns boolean
   Object.values(users).forEach((user) => {
-    if(user.username === username) {
+    if (user.username === username) {
       return true;
     }
   });
 
   return false;
-}
+};
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+const authenticatedUser = (username, password) => {
+  return Object.values(users).some(
+    (user) => user.username === username && user.password === password
+  );
+};
 
 // Only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post("/login", (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+
+  if ((!username || !password) || !authenticatedUser(username, password)) {
+    return res.status(400).json({ message: "Invalid username or password" });
+  }
+
+  let token = jwt.sign({ username: username }, TOKEN_HEADER_KEY, {
+    expiresIn: 60 * 60,
+  });
+  req.session.authorization = token;
+
+  return res.status(200).json({ message: "Login successful" });
+});
+
+// Register a user
+regd_users.post("/register", (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: "Invalid username or password" });
+  }
+
+  if (isValid(username)) {
+    return res.status(400).json({ message: "Username already exists" });
+  }
+
+  users.push({ username: username, password: password });
+
+  return res
+    .status(200)
+    .json({ message: "User successfully registered. Now you can login" });
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 module.exports.authenticated = regd_users;
